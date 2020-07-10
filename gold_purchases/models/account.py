473a023d0bd00c7e48b748cf0e8f0212cfabfcfc
kalpesh <kalpesh.gajera@sprintit.fi>
2020-07-10 10:20:48 +0530

    Hide gold journal and it's entry from every where in the system.

--- gold_purchases/models/account.py
+++ gold_purchases/models/account.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from odoo.osv import expression
 from odoo import api, fields, models
 
 
@@ -12,3 +13,17 @@ class AccountJournal(models.Model):
     _inherit = 'account.journal'
 
     gold = fields.Boolean('Gold Journal')
+
+
+class AccountReport(models.AbstractModel):
+    _inherit = 'account.report'
+
+    @api.model
+    def _query_get(self, options, domain=None):
+        domain = self._get_options_domain(options) + (domain or [])
+        self.env['account.move.line'].check_access_rights('read')
+        domain = expression.AND([domain, [('journal_id.gold', '=', False)]])
+        query = self.env['account.move.line']._where_calc(domain)
+        # Wrap the query with 'company_id IN (...)' to avoid bypassing company access rights.
+        self.env['account.move.line']._apply_ir_rules(query)
+        return query.get_sql()
