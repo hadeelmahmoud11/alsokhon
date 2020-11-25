17a16595e99c330f0ec5423869848979f913fc70
Hashem Aly <hashem.aly@core-bpo.com>
2020-11-26 01:06:11 +0200

    [IMP] gold_purchases: add search and groupby to computed fields in stock quant

--- gold_purchases/models/stock_quant.py
+++ gold_purchases/models/stock_quant.py
@@ -22,12 +22,15 @@ class StockQuant(models.Model):
                                compute="_compute_move_line_values", )
 
     item_category_id = fields.Many2one('item.category', string="Item Category",
-                                       compute="_compute_move_line_values", )
+                                       compute="_compute_move_line_values",
+                                       search="_search_item_category_id")
     sub_category_id = fields.Many2one('item.category.line',
                                       string="Sub Category",
+                                      search="_search_sub_category_id",
                                       compute="_compute_move_line_values", )
     selling_karat_id = fields.Many2one('product.attribute.value',
                                        string="Selling Karat",
+                                       search="_search_selling_karat_id",
                                        compute="_compute_move_line_values", )
     selling_making_charge = fields.Monetary('Selling Making Charge',
                                             digits=(16, 3),
@@ -51,7 +54,7 @@ class StockQuant(models.Model):
                 move_line_id = move_line_id.search([
                     ('lot_id', '=', rec.lot_id.id),
                     ('product_id', '=', rec.product_id.id)
-                ])
+                ], limit=1)
                 if move_line_id:
                     gross_weight = move_line_id.gross_weight
                     purity = move_line_id.purity
@@ -68,3 +71,99 @@ class StockQuant(models.Model):
             rec.selling_karat_id = selling_karat_id
             rec.selling_making_charge = selling_making_charge
             rec.move_line_id = move_line_id
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None,
+                   orderby=False, lazy=True):
+        """
+        Override read_group
+        """
+        result = super(StockQuant, self).read_group(domain, fields, groupby,
+                                                    offset=offset, limit=limit,
+                                                    orderby=orderby, lazy=lazy)
+        quants = self.env['stock.quant']
+        for line in result:
+            if '__domain' in line:
+                quants = self.search(line['__domain'])
+            if 'gross_weight' in fields:
+                line['gross_weight'] = sum(quants.mapped('gross_weight'))
+            if 'purity' in fields:
+                line['purity'] = sum(quants.mapped('purity'))
+            if 'pure_weight' in fields:
+                line['pure_weight'] = sum(quants.mapped('pure_weight'))
+            if 'selling_making_charge' in fields:
+                line['selling_making_charge'] = sum(quants.mapped(
+                    'selling_making_charge'))
+        return result
+    def _search_item_category_id(self, operator, value):
+        """
+        Search item_category_id
+        """
+        ids = []
+        quants = self.env['stock.quant'].search([])
+        if isinstance(value, int):
+            ids = quants.filtered(lambda q: q.item_category_id.id == value).ids
+        elif isinstance(value, str):
+            ic_ids = self.env['item.category'].search(
+                [('name', 'ilike', value)]).ids
+            ids = quants.filtered(
+                lambda q: q.item_category_id and q.item_category_id.id in ic_ids
+            ).ids
+        elif isinstance(value, bool):
+            if operator == '=' and value or operator == '!=' and not value:
+                ids = quants.filtered(lambda q: q.item_category_id).ids
+            else:
+                ids = quants.filtered(lambda q: not q.item_category_id).ids
+        if ids:
+            return [('id', 'in', ids)]
+        return []
+
+    def _search_sub_category_id(self, operator, value):
+        """
+        Search sub_category_id
+        """
+        ids = []
+        quants = self.env['stock.quant'].search([])
+        if isinstance(value, int):
+            ids = quants.filtered(lambda q: q.sub_category_id.id == value).ids
+        elif isinstance(value, str):
+            icl_ids = self.env['item.category.line'].search(
+                [('name', 'ilike', value)]).ids
+            ids = quants.filtered(
+                lambda q: q.sub_category_id and q.sub_category_id.id in icl_ids
+            ).ids
+        elif isinstance(value, bool):
+            if operator == '=' and value or operator == '!=' and not value:
+                ids = quants.filtered(lambda q: q.sub_category_id).ids
+            else:
+                ids = quants.filtered(lambda q: not q.sub_category_id).ids
+        if ids:
+            return [('id', 'in', ids)]
+        return []
+
+
+    def _search_selling_karat_id(self, operator, value):
+        """
+        Search selling_karat_id
+        """
+        ids = []
+        quants = self.env['stock.quant'].search([])
+        if isinstance(value, int):
+            ids = quants.filtered(lambda q: q.selling_karat_id.id == value).ids
+        elif isinstance(value, str):
+            pavids = self.env['product.attribute.value'].search(
+                [('name', 'ilike', value)]).ids
+            ids = quants.filtered(
+                lambda q: q.selling_karat_id and q.selling_karat_id.id in pavids
+            ).ids
+        elif isinstance(value, bool):
+            if operator == '=' and value or operator == '!=' and not value:
+                ids = quants.filtered(lambda q: q.selling_karat_id).ids
+            else:
+                ids = quants.filtered(lambda q: not q.selling_karat_id).ids
+        if ids:
+            return [('id', 'in', ids)]
+        return []
+
+
+
