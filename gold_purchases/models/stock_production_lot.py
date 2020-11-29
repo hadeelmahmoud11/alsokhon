69e9045f6871d5c38bc809bd71b29e7318e8144a
anabil <anabil@white-code.co.uk>
2020-11-30 01:53:17 +0200

    remain Weight

--- gold_purchases/models/stock_production_lot.py
+++ gold_purchases/models/stock_production_lot.py
@@ -14,11 +14,25 @@ class StockProductionLot(models.Model):
     selling_karat_id = fields.Many2one('product.attribute.value', string="Selling Karat")
     selling_making_charge = fields.Monetary('Selling Making Charge')
     currency_id = fields.Many2one('res.currency', string="Company Currency", related='company_id.currency_id')
+    remaining_weight = fields.Float(compute='get_remain_weight',string="Remaining Weight", store=True, digits=(16, 3))
 
     @api.depends('gross_weight', 'purity')
     def get_pure_weight(self):
         for rec in self:
-            rec. pure_weight = rec.gross_weight * rec.purity / 1000
+            rec.pure_weight = rec.gross_weight * rec.purity / 1000
     
+    @api.depends('gross_weight', 'purity')
+    def get_remain_weight(self):
+        prev_weight_out = self.env["stock.move.line"].search([('lot_id','=',self.id),('location_id','=',8)])
+        prev_weight_in = self.env["stock.move.line"].search([('lot_id','=',self.id),('location_dest_id','=',8)])
+        tot_prev_out = 0
+        tot_prev_in = 0
+        for i in prev_weight_out:
+            tot_prev_out += i.pure_weight
+        for n in prev_weight_in:
+            tot_prev_in += n.pure_weight
+        for rec in self:
+            rec.remaining_weight += tot_prev_in-tot_prev_out
+        
 
     
