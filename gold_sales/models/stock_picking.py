56030ba7a8ecff65103d3a184279e1b252830a3f
anabil <anabil@white-code.co.uk>
2020-12-08 11:11:39 +0200

    fix gold entry

--- gold_sales/models/stock_picking.py
+++ gold_sales/models/stock_picking.py
@@ -31,94 +31,94 @@ class StockPicking(models.Model):
     #     #             rec.create_unfixed_journal_entry()
     #     return res
     #
-     def create_gold_journal_entry(self):
-         self.ensure_one()
-         if 'P0' in self.group_id.name:
-             purchase_obj = self.env['purchase.order'].search([('name','=',self.group_id.name)])
-             moves = self.move_lines.filtered(lambda x: x._is_in() and
+    def create_gold_journal_entry(self):
+        self.ensure_one()
+        if 'P0' in self.group_id.name:
+            purchase_obj = self.env['purchase.order'].search([('name','=',self.group_id.name)])
+            moves = self.move_lines.filtered(lambda x: x._is_in() and
                                                         x.product_id and
                                                         x.product_id.gold and
                                                         x.product_id.categ_id and
                                                         x.product_id.categ_id.is_gold and
                                                         x.product_id.categ_id.gold_on_hand_account)
-             if moves:
-                 total_purity = 0
-                 product_dict = {}
-                 description = '%s' % self.name
-                 for product_id, move_list in groupby(moves, lambda x: x.product_id):
-                     description = '%s-%s' % (description, product_id.display_name)
-                     if product_id not in product_dict.keys():
-                         product_dict[product_id] = sum(
-                             x.pure_weight for x in move_list)
-                     else:
-                         product_dict[product_id] = product_dict[product_id] + sum(
-                             x.pure_weight for x in move_list)
-                 total_purity = sum(value for key, value in product_dict.items())
-                 if total_purity > 0.0 and product_dict and \
-                         self.partner_id :
-                     if not next(iter(product_dict)).categ_id.gold_journal.id:
-                         raise ValidationError(_('Please fill gold journal in product Category'))
-                     journal_id = next(iter(product_dict)).categ_id.gold_journal.id
+            if moves:
+                total_purity = 0
+                product_dict = {}
+                description = '%s' % self.name
+                for product_id, move_list in groupby(moves, lambda x: x.product_id):
+                    description = '%s-%s' % (description, product_id.display_name)
+                    if product_id not in product_dict.keys():
+                        product_dict[product_id] = sum(
+                            x.pure_weight for x in move_list)
+                    else:
+                        product_dict[product_id] = product_dict[product_id] + sum(
+                            x.pure_weight for x in move_list)
+                total_purity = sum(value for key, value in product_dict.items())
+                if total_purity > 0.0 and product_dict and \
+                        self.partner_id :
+                    if not next(iter(product_dict)).categ_id.gold_journal.id:
+                        raise ValidationError(_('Please fill gold journal in product Category'))
+                    journal_id = next(iter(product_dict)).categ_id.gold_journal.id
     #
-                     move_lines = self._prepare_account_move_line(product_dict)
-                     if move_lines:
-                         AccountMove = self.env['account.move'].with_context(
-                             default_journal_id=journal_id)
-                         date = self._context.get('force_period_date',
+                    move_lines = self._prepare_account_move_line(product_dict)
+                    if move_lines:
+                        AccountMove = self.env['account.move'].with_context(
+                            default_journal_id=journal_id)
+                        date = self._context.get('force_period_date',
                                                   fields.Date.context_today(self))
-                         new_account_move = AccountMove.sudo().create({
+                        new_account_move = AccountMove.sudo().create({
                              'journal_id': journal_id,
                              'line_ids': move_lines,
                              'date': date,
                              'ref': description,
                              'type': 'entry',
-                         })
-                         new_account_move.post()
-                         if purchase_obj:
-                             purchase_obj.write({'stock_move_id': new_account_move.id})
-         elif 'S0' in self.origin:
-             sale_obj = self.env['sale.order'].search([('name','=',self.origin)])
-             moves = self.move_lines.filtered(lambda x: x._is_out() and
+                        })
+                        new_account_move.post()
+                        if purchase_obj:
+                            purchase_obj.write({'stock_move_id': new_account_move.id})
+        elif 'S0' in self.origin:
+            sale_obj = self.env['sale.order'].search([('name','=',self.origin)])
+            moves = self.move_lines.filtered(lambda x: x._is_out() and
                                                         x.product_id and
                                                         x.product_id.gold and
                                                         x.product_id.categ_id and
                                                         x.product_id.categ_id.is_gold and
                                                         x.product_id.categ_id.gold_on_hand_account)
-             if moves:
-                 total_purity = 0
-                 product_dict = {}
-                 description = '%s' % self.name
-                 for product_id, move_list in groupby(moves, lambda x: x.product_id):
-                     description = '%s-%s' % (description, product_id.display_name)
-                     if product_id not in product_dict.keys():
-                         product_dict[product_id] = sum(
-                             x.pure_weight for x in move_list)
-                     else:
-                         product_dict[product_id] = product_dict[product_id] + sum(
-                             x.pure_weight for x in move_list)
-                 total_purity = sum(value for key, value in product_dict.items())
-                 if total_purity > 0.0 and product_dict and \
-                         self.partner_id :
-                     if not next(iter(product_dict)).categ_id.gold_journal.id:
-                         raise ValidationError(_('Please fill gold journal in product Category'))
-                     journal_id = next(iter(product_dict)).categ_id.gold_journal.id
+            if moves:
+                total_purity = 0
+                product_dict = {}
+                description = '%s' % self.name
+                for product_id, move_list in groupby(moves, lambda x: x.product_id):
+                    description = '%s-%s' % (description, product_id.display_name)
+                    if product_id not in product_dict.keys():
+                        product_dict[product_id] = sum(
+                            x.pure_weight for x in move_list)
+                    else:
+                        product_dict[product_id] = product_dict[product_id] + sum(
+                            x.pure_weight for x in move_list)
+                total_purity = sum(value for key, value in product_dict.items())
+                if total_purity > 0.0 and product_dict and \
+                        self.partner_id :
+                    if not next(iter(product_dict)).categ_id.gold_journal.id:
+                        raise ValidationError(_('Please fill gold journal in product Category'))
+                    journal_id = next(iter(product_dict)).categ_id.gold_journal.id
     #
-                     move_lines = self._prepare_account_move_line(product_dict)
-                     if move_lines:
-                         AccountMove = self.env['account.move'].with_context(
+                    move_lines = self._prepare_account_move_line(product_dict)
+                    if move_lines:
+                        AccountMove = self.env['account.move'].with_context(
                              default_journal_id=journal_id)
-                         date = self._context.get('force_period_date',
+                        date = self._context.get('force_period_date',
                                                   fields.Date.context_today(self))
-                         new_account_move = AccountMove.sudo().create({
+                        new_account_move = AccountMove.sudo().create({
                              'journal_id': journal_id,
                              'line_ids': move_lines,
                              'date': date,
                              'ref': description,
                              'type': 'entry',
-                         })
-                         new_account_move.post()
-                         if sale_obj:
-                             sale_obj.write({'stock_move_id': new_account_move.id})
+                        })
+                        new_account_move.post()
+                        if sale_obj:
+                            sale_obj.write({'stock_move_id': new_account_move.id})
     #
     # def _prepare_account_move_line(self, product_dict):
     #     if 'P0' in self.group_id.name:
