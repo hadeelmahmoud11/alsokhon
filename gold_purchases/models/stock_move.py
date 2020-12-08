bbb5d7b1fe85365902d29551dddd86388c702c16
anabil <anabil@white-code.co.uk>
2020-12-08 10:41:23 +0200

    valuation out

--- gold_purchases/models/stock_move.py
+++ gold_purchases/models/stock_move.py
@@ -83,37 +83,7 @@ class StockMove(models.Model):
         return stock_val_layer
     
 
-    def _create_out_svl(self, forced_quantity=None):
-        """Create a `stock.valuation.layer` from `self`.
-
-        :param forced_quantity: under some circunstances, the quantity to value is different than
-            the initial demand of the move (Default value = None)
-        """
-        svl_vals_list = []
-        for move in self:
-            move = move.with_context(force_company=move.company_id.id)
-            valued_move_lines = move._get_out_move_lines()
-            valued_quantity = 0
-            for valued_move_line in valued_move_lines:
-                valued_quantity += valued_move_line.product_uom_id._compute_quantity(valued_move_line.qty_done, move.product_id.uom_id)
-            if float_is_zero(forced_quantity or valued_quantity, precision_rounding=move.product_id.uom_id.rounding):
-                continue
-             # Check Gold Product and pass gold rate, pure weight instead of
-            # cost, qty
-            if move.product_id.gold:
-                svl_vals = move.product_id._prepare_out_svl_vals(move.pure_weight, move.company_id)
-                svl_vals['unit_cost'] = 0.00 
-                svl_vals['quantity'] = 0.00 
-                svl_vals['value'] = 0.00  
-            else:
-                svl_vals = move.product_id._prepare_out_svl_vals(forced_quantity or valued_quantity, move.company_id)
-            svl_vals.update(move._prepare_common_svl_vals())
-            if forced_quantity:
-                svl_vals['description'] = 'Correction of %s (modification of past move)' % move.picking_id.name or move.name
-            
-            svl_vals_list.append(svl_vals)
-        return self.env['stock.valuation.layer'].sudo().create(svl_vals_list)
-
+    
 
     def _action_done(self, cancel_backorder=False):
         res = super(StockMove, self)._action_done()
