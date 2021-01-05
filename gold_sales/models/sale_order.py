d7e7be94f4148c7eb99bd4d31fce800f4ec6b658
Abdel-Rahman-Warda <awarda@white-code.co.uk>
2021-01-05 21:00:55 +0200

    Update sale_order.py

--- gold_sales/models/sale_order.py
+++ gold_sales/models/sale_order.py
@@ -152,6 +152,10 @@ class SaleOrderLine(models.Model):
     @api.onchange('lot_id')
     def _get_making(self):
         if self.lot_id and self.product_id:
+            if self.product_id.categ_id.is_scrap:
+                self.product_uom_qty = self.lot_id.product_qty
+            if not self.product_id.categ_id.is_scrap:
+                self.purity_id = self.lot_id.purity_id.id
             self.make_rate = self.lot_id.selling_making_charge
             self.price_unit = 0
             # print(self.lot_id.gross_weight)
