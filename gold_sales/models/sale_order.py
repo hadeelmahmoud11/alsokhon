f4ddbdb534238d2109e79a30352e8931ef481c89
Abdel-Rahman-Warda <75036891+Abdel-Rahman-Warda@users.noreply.github.com>
2021-01-12 16:07:07 +0200

    Update sale_order.py

--- gold_sales/models/sale_order.py
+++ gold_sales/models/sale_order.py
@@ -496,13 +496,8 @@ class SaleOrderLine(models.Model):
                     line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
             elif line.order_id.diamond:
                 # price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                #
-                price = 0
-                price_unit = line.price_unit
-                product_uom_qty = line.product_uom_qty
-                discount = line.discount
-                price = (price_unit * product_uom_qty) - ((price_unit * discount / 100) * product_uom_qty),
-                taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+                price = (line.price_unit * line.product_uom_qty) - ((line.price_unit * line.discount / 100) * line.product_uom_qty),
+                taxes = line.tax_id.compute_all(price[0], line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
                 line.update({
                     'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
                     'price_total': taxes['total_included'],
