a680762c1a22d09df1d1d00aa83947993fed696b
Abdel-Rahman-Warda <75036891+Abdel-Rahman-Warda@users.noreply.github.com>
2021-01-12 13:31:03 +0200

    Update stock_move.py

--- gold_purchases/models/stock_move.py
+++ gold_purchases/models/stock_move.py
@@ -81,7 +81,7 @@ class StockMove(models.Model):
             elif move.product_id.diamond:
                 purchase_order = self.env['purchase.order']
                 if 'P0' in move.origin:
-                    stock_picking = self.env['stock.picking'].search(['name','=',move.origin])
+                    stock_picking = self.env['stock.picking'].search([('name','=',move.origin)])
                     purchase_order.search([('name','=',stock_picking.origin)])
                     if len(purchase_order) > 0:
                         pol = self.env['purchase.order.line'].search([('order_id','=',purchase_order.id),('product_id','=',move.product_id.id)])
