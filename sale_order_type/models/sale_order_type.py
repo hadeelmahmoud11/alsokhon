5cae472dee1b577882f78358b44e97087bb1d75e
Abdel-Rahman-Warda <75036891+Abdel-Rahman-Warda@users.noreply.github.com>
2021-01-19 13:31:06 +0200

    assembly gold sale

--- sale_order_type/models/sale_order_type.py
+++ sale_order_type/models/sale_order_type.py
@@ -32,6 +32,7 @@ class SaleOrderType(models.Model):
     is_unfixed = fields.Boolean('Is Unfixed')
     gold = fields.Boolean('Gold')
     diamond = fields.Boolean('Diamond')
+    assembly = fields.Boolean('Assembly')
     stock_picking_type_id = fields.Many2one(
         comodel_name='stock.picking.type', string='picking type')
 
